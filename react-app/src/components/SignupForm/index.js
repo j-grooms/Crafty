b9f0c1d5e802770eeed26d0432c5e206c3e0b5fa
Jacob Grooms <grooms.jacob@gmail.com>
2021-01-08 12:59:19 -0500

    starting image fields on sign up form

--- react-app/src/components/SignupForm/index.js
+++ react-app/src/components/SignupForm/index.js
@@ -3,17 +3,19 @@ import { Redirect } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
 const SignupForm = () => {
-    const currentUser = useSelector((state) => state.session.user);
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    const [bio, setBio] = useState("");
-    const [email, setEmail] = useState("");
+	const currentUser = useSelector((state) => state.session.user);
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+	const [bio, setBio] = useState("");
+	const [email, setEmail] = useState("");
 	const [profilePic, setProfilePic] = useState("");
 	const [profileUrl, setProfileUrl] = useState("");
 	const [bannerPic, setBannerPic] = useState("");
-    const [bannerUrl, setBannerUrl] = useState("");
-    const [money, setMoney] = useState(0);
+	const [bannerUrl, setBannerUrl] = useState("");
+	const [money, setMoney] = useState(0);
+
+	if (currentUser) return <Redirect to="/shop" />;
 
 	const handleBanner = (e) => {
 		const file = e.target.files[0];
@@ -41,11 +43,74 @@ const SignupForm = () => {
 		}
 	};
 
-    if (currentUser) return <Redirect to='/shop' />
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+	};
 
-    return (
-        <p>Test</p>
-    )
+	return (
+		<>
+			<form enctype="multipart/form-data">
+				<div>
+					<label htmlFor="username">Username</label>
+					<input
+						required
+						type="text"
+						name="username"
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
+					/>
+				</div>
+				<div>
+					<label htmlFor="email">Email</label>
+					<input
+						required
+						type="email"
+						name="email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+				</div>
+				<div>
+					<label htmlFor="password">Password</label>
+					<input
+						required
+						type="password"
+						name="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</div>
+				<div>
+					<label htmlFor="confirmPassword">Confirm Password</label>
+					<input
+						required
+						type="password"
+						name="confirmPassword"
+						value={confirmPassword}
+						onChange={(e) => setConfirmPassword(e.target.value)}
+					/>
+				</div>
+				<div>
+					<label htmlFor="bio">Bio</label>
+					<textarea
+						name="bio"
+						placeholder="Tell us about yourself, or your products!"
+						value={bio}
+						onChange={(e) => setBio(e.target.value)}
+					/>
+				</div>
+				<div>
+					<label htmlFor="money">Money (fake, but free!)</label>
+					<input
+						type="number"
+						step="any"
+						value={money}
+						onChange={(e) => setMoney(e.target.value)}
+					/>
+				</div>
+			</form>
+		</>
+	);
 };
 
 export default SignupForm;
