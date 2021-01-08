dcf5fac996074b685df23076f42477acccb5f3f9
Jacob Grooms <grooms.jacob@gmail.com>
2021-01-08 12:22:05 -0500

    variables set for signup

--- react-app/src/components/SignupForm/index.js
+++ react-app/src/components/SignupForm/index.js
@@ -4,8 +4,42 @@ import { useDispatch, useSelector } from "react-redux";
 
 const SignupForm = () => {
     const currentUser = useSelector((state) => state.session.user);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [bio, setBio] = useState("");
+    const [email, setEmail] = useState("");
+	const [profilePic, setProfilePic] = useState("");
+	const [profileUrl, setProfileUrl] = useState("");
+	const [bannerPic, setBannerPic] = useState("");
+    const [bannerUrl, setBannerUrl] = useState("");
+    const [money, setMoney] = useState(0);
 
+	const handleBanner = (e) => {
+		const file = e.target.files[0];
+		const fileReader = new FileReader();
+		setBannerPic(file);
+		console.log(bannerPic);
+		if (file) {
+			fileReader.readAsDataURL(file);
+			fileReader.onloadend = () => {
+				setBannerUrl(fileReader.result);
+			};
+		}
+	};
 
+	const handleProfile = (e) => {
+		const file = e.target.files[0];
+		const fileReader = new FileReader();
+		setProfilePic(file);
+		console.log(profilePic);
+		if (file) {
+			fileReader.readAsDataURL(file);
+			fileReader.onloadend = () => {
+				setProfileUrl(fileReader.result);
+			};
+		}
+	};
 
     if (currentUser) return <Redirect to='/shop' />
 
