987bb73b4edfc49a26cee57f5bb4364d9a06a862
Jacob Grooms <grooms.jacob@gmail.com>
2021-01-09 16:16:00 -0500

    debugging favorite button

--- react-app/src/components/FavoriteButton/index.js
+++ react-app/src/components/FavoriteButton/index.js
@@ -1,35 +1,39 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
-import { addFavorite, removeFavorite } from "../../store/session"
-import { checkFavorite } from "../../store/favorite"
+import { addFavorite, removeFavorite } from "../../store/session";
+import { checkFavorite } from "../../store/favorite";
 
 const FavoriteButton = () => {
-	const currentFavorites = useSelector((state) => state.session.user.favorites);
-	const currentUser = useSelector((state) => state.session.user)
-    const [isFavorite, setIsFavorite] = useState(false);
+	const currentFavorites = useSelector((state) => state.favorites.favorites);
+	const currentUser = useSelector((state) => state.session.user);
+	const [isFavorite, setIsFavorite] = useState(false);
 	const product = useSelector((state) => state.products.product);
 
-    const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-		useEffect(() => {
-			(async () => {
-				for (let i = 0; i < currentFavorites.length; i++) {
-					let favorite = currentFavorites[i];
-					if (favorite.product.id === product.id) await setIsFavorite(true);
-				}
-			})();
-		}, [currentFavorites, currentUser]);
+	useEffect(() => {
+		(async () => {
+			for (let i = 0; i < currentFavorites.length; i++) {
+				let favorite = currentFavorites[i];
+				if (favorite.product.id === product.id) await setIsFavorite(true);
+			}
+		})();
+	}, [currentFavorites, isFavorite]);
 
 	const handleFavorite = () => {
 		if (isFavorite) return dispatch(removeFavorite(product.id, currentUser.id));
-		return dispatch(addFavorite(product.id, currentUser.id))
+		return dispatch(addFavorite(product.id, currentUser.id));
 	};
 
 	return (
-        <>
-            {isFavorite ? <button onClick={handleFavorite}>remove favorite</button> : <button onClick={handleFavorite}>add favorite</button>}
-        </>
-    );
+		<>
+			{isFavorite ? (
+				<button onClick={handleFavorite}>remove favorite</button>
+			) : (
+				<button onClick={handleFavorite}>add favorite</button>
+			)}
+		</>
+	);
 };
 
 export default FavoriteButton;
