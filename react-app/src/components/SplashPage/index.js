58c93fc854a7753f696401cf1d273e1124131f8c
Jacob Grooms <grooms.jacob@gmail.com>
2021-01-14 13:46:36 -0500

    splash functionality working, need styling

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,23 +1,36 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
 import LoginForm from "../LoginForm";
 import SignupForm from "../SignupForm";
 import Modal from "../Modal";
 import "./SplashPage.css";
 
 const SplashPage = () => {
-    const [login, setLogin] = useState(false);
-    const [signup, setSignup] = useState(false);
+	const [login, setLogin] = useState(false);
+	const [signup, setSignup] = useState(false);
+	const currentUser = useSelector((state) => state.session.user);
+
+	if (currentUser) return <Redirect to="/shop" />;
+
 	return (
 		<div className="splash-background">
 			<div className="forms-holder">
-				<button onClick={() => setLogin(true)}>Sign in</button>
-				<button onClick={() => setSignup(true)}>Sign up</button>
-                <Modal open={login} onClose={() => setLogin(false)}>
-                    <LoginForm />
-                </Modal>
-                <Modal open={signup} onClose={() => setSignup(false)}>
-                    <SignupForm />
-                </Modal>
+				<button className="splash-option" onClick={() => setLogin(true)}>
+					Have an Account?
+				</button>
+				<button className="splash-option" onClick={() => setSignup(true)}>
+					New User?
+				</button>
+				<Modal open={login} onClose={() => setLogin(false)}>
+					<LoginForm />
+				</Modal>
+				<Modal open={signup} onClose={() => setSignup(false)}>
+					<SignupForm />
+				</Modal>
+			</div>
+			<div className="splash-about">
+				<p>Welcome to Crafty</p>
 			</div>
 		</div>
 	);
