0355e4b304913e979eaec4d03cb93304c27a17ff
Jacob Grooms <grooms.jacob@gmail.com>
2021-01-14 16:21:39 -0500

    user delete form styled

--- react-app/src/components/UserDeleteForm/index.js
+++ react-app/src/components/UserDeleteForm/index.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { deleteUser } from '../../store/session'
+import { deleteUser } from "../../store/session";
 
 const UserDeleteForm = () => {
 	const currentUser = useSelector((state) => state.session.user);
@@ -12,20 +12,24 @@ const UserDeleteForm = () => {
 
 	const handleSubmit = async (event) => {
 		event.preventDefault();
-        const formData = { username, password };
-        await dispatch(deleteUser(formData, currentUser.id));
-        return history.push('/signup')
+		const formData = { username, password };
+		await dispatch(deleteUser(formData, currentUser.id));
+		return history.push("/signup");
 	};
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<p>
-				WARNING: This will delete your account, as well as all product listings
-				and reviews
-			</p>
+		<form className="deletion-form" onSubmit={handleSubmit}>
+			<div className="warning-div">
+				<p className="warning-header">WARNING</p>
+				<p className="warning-text">
+					This will delete your account, as well as all product listings and
+					reviews
+				</p>
+			</div>
 			<div>
-				<label htmlFor="username">Username</label>
+				<label className="form-label" htmlFor="username">Username</label>
 				<input
+					className="warning-input"
 					type="text"
 					name="username"
 					value={username}
@@ -33,17 +37,18 @@ const UserDeleteForm = () => {
 				/>
 			</div>
 			<div>
-				<label htmlFor="password">Password</label>
+				<label className="form-label" htmlFor="password">Password</label>
 				<input
+					className="warning-input"
 					type="password"
 					name="password"
 					value={password}
 					onChange={(e) => setPassword(e.target.value)}
 				/>
 			</div>
-            <div>
-                <button type="submit">Delete Account</button>
-            </div>
+			<div>
+				<button className="warning-button" type="submit">Delete Account</button>
+			</div>
 		</form>
 	);
 };
