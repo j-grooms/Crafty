0f15ccdf3882e85f446709a8e18932508128f7f6
Jacob Grooms <grooms.jacob@gmail.com>
2021-01-23 21:51:07 -0500

    profile page overhaul

--- server/react-app/src/components/ProfilePage/index.js
+++ server/react-app/src/components/ProfilePage/index.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getSeller } from "../../store/seller";
 import { fetchAllProductsBySeller } from "../../store/product";
@@ -12,10 +12,11 @@ import "./ProfilePage.css";
 
 const ProfilePage = () => {
 	const currentUser = useSelector((state) => state.session.user);
+	const following = useSelector((state) => state.session.user.following);
 	const seller = useSelector((state) => state.seller.seller);
 	const products = useSelector((state) => state.products.products);
 	const favorites = useSelector((state) => state.favorites.favorites);
-	const [editing, setEditing] = useState(false)
+	const [editing, setEditing] = useState(false);
 	const [loaded, setLoaded] = useState(false);
 	const isSeller = currentUser.id === seller.id;
 	const dispatch = useDispatch();
@@ -30,6 +31,23 @@ const ProfilePage = () => {
 			return setLoaded(true);
 		})();
 	}, [id, currentUser, currentUser.id, dispatch]);
+
+	const generateLinks = () => {
+		if (!following.length) return [<p>You are not following any users!</p>];
+		const links = following.map((user, index) => (
+			<div className="profile-following-link-container">
+				<Link
+					className="profile-following-link"
+					key={index}
+					to={`/user/${user.id}`}
+				>
+					<p>{user.username}</p>
+					<i className="fas fa-arrow-circle-right profile-following-link-icon"></i>
+				</Link>
+			</div>
+		));
+		return links;
+	};
 
 	return (
 		loaded && (
@@ -54,9 +72,14 @@ const ProfilePage = () => {
 							<p className="profile-username">{seller.username}</p>
 							{isSeller ? (
 								<>
-									<button className="login-button" onClick={() => setEditing(true)}>Edit Info</button>
+									<button
+										className="login-button"
+										onClick={() => setEditing(true)}
+									>
+										Edit Info
+									</button>
 									<Modal open={editing} onClose={() => setEditing(false)}>
-										<UserEditForm onClose={() => setEditing(false)}/>
+										<UserEditForm onClose={() => setEditing(false)} />
 									</Modal>
 								</>
 							) : (
@@ -64,13 +87,27 @@ const ProfilePage = () => {
 							)}
 						</div>
 						<div className="profile-info-container">
+							{isSeller ? 	<div>
+									<p className="profile-bio">
+										Money in your wallet: ${seller.money}
+									</p>
+								</div> : <></>}
 							<p className="profile-info-header">About this Seller</p>
-							{isSeller? <p className="profile-bio">Money in your wallet: ${seller.money}</p> : <></>}
 							<p className="profile-bio">{seller.bio}</p>
+							{isSeller ? (
+								<div>
+									<p className="profile-info-header">People You Follow</p>
+									{generateLinks().map((link) => link)}
+								</div>
+							) : (
+								<></>
+							)}
 						</div>
 					</div>
 					<div className="profile-products-grid-div">
-						<p className="profile-products-header">{isSeller ? "Your Products": "Products by this User"}</p>
+						<p className="profile-products-header">
+							{isSeller ? "Your Products" : "Products by this User"}
+						</p>
 						<div className="profile-products-container">
 							{products.map((product, i) => (
 								<ProductPreview
