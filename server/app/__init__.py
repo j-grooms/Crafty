8894b78f3b25bee4660a0f7777d902d631abf934
Jacob Grooms <grooms.jacob@gmail.com>
2021-01-25 15:54:55 -0500

    resolving all warnings

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -68,8 +68,8 @@ def inject_csrf_token(response):
 @app.route('/<path:path>')
 def react_root(path):
     print("path", path)
-    if path == 'favicon.ico':
-        return app.send_static_file('favicon.ico')
+    if path in ('favicon.ico', 'site.webmanifest', 'manifest.json'):
+        return app.send_static_file(path)
     return app.send_static_file('index.html')
 
 
