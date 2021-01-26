95a6434282d02b1d45d9ae089f6fa4da448291d7
Jacob Grooms <grooms.jacob@gmail.com>
2021-01-26 18:37:05 -0500

    tweaking search results

--- server/react-app/src/components/SearchResults/index.js
+++ server/react-app/src/components/SearchResults/index.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { searchByTag, fetchAllProducts } from "../../store/search";
 import ProductPreview from "../ProductPreview";
+import "./SearchResults.css";
 
 const SearchResults = () => {
 	const [loaded, setLoaded] = useState(false);
@@ -13,8 +14,8 @@ const SearchResults = () => {
 	useEffect(() => {
 		(async () => {
 			if (term === "all") {
-                await dispatch(fetchAllProducts());
-                return setLoaded(true);
+				await dispatch(fetchAllProducts());
+				return setLoaded(true);
 			} else {
 				await dispatch(searchByTag(term));
 				return setLoaded(true);
@@ -26,6 +27,11 @@ const SearchResults = () => {
 		loaded && (
 			<>
 				<div className="feed-header">Your Search Results</div>
+				{!results.length ? (
+					<p className="no-results">Your search did not return any results.</p>
+				) : (
+					<></>
+				)}
 				<div className="feed-container">
 					{results.map((product, i) => (
 						<ProductPreview key={product.name.concat(i)} product={product} />
